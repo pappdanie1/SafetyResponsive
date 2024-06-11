87b0b12df351a39678d9a0faaa1d4feb3700cca5
Papp Dániel <pappdani2@gmail.com>
2024-06-11 20:34:07 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import React from 'react'
+import Home from './pages/Home'
+import Header from './components/Header'
+import Contact from './pages/Contact'
+import OccupationalSafety from './pages/OccupationalSafety'
+import FireSafety from './pages/FireSafety'
+import EnvironmentalProtection from './pages/EnvironmentalProtection'
+import NotFound from './pages/NotFound'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return(
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/kapcsolat' element={<Contact/>}/>
+                <Route path='/munkavedelem' element={<OccupationalSafety/>}/>
+                <Route path='/tuzvedelem' element={<FireSafety/>}/>
+                <Route path='/kornyezetvedelem' element={<EnvironmentalProtection/>}/>
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
