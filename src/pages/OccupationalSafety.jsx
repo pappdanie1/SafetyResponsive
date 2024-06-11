250ea85e628129129810ca4fb8dbc3e996212156
Papp Dániel <pappdani2@gmail.com>
2024-06-11 20:36:06 +0200

    OccupationalSafety page

---
+++ src/pages/OccupationalSafety.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OccupationalSafety = () => {
+
+    return (
+        <>
+            Occupational Safety
+        </>
+    )
+}
+
+export default OccupationalSafety
