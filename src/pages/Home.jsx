f45d02c66e5c9bf1c4fd94c5d620e9cef118ae8b
Papp Dániel <pappdani2@gmail.com>
2024-06-11 20:35:27 +0200

    Homepage with slider

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Carousel from "../components/Carousel";
+
+const Home = () => {
+
+    return (
+        <>
+            <Carousel/>
+            <div>
+            </div>
+        </>
+
+    )
+}
+
+export default Home
