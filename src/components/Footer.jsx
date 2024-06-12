dde50e87fafdc898f5dd1a21dfbc1b840c0f4f0f
Papp Dániel <pappdani2@gmail.com>
2024-06-12 15:58:05 +0200

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { IoPhonePortraitOutline } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
+import '../css/Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="contact">
+                <h3>Kapcsolat</h3>
+                <p>Email:</p>
+                <a href="mailto:pappzsolt6903@gmail.com">pappzsolt6903@gmail.com</a>
+                <p>Telefon:</p>
+                <a href="tel:+36308647319">+36 30 8647319</a>
+            </div>
+            <div className="links">
+                <h3>Linkek</h3>
+                <ul>
+                    <li>
+                        <a href="/">Kezdőlap</a>
+                    </li>
+                    <li>
+                        <a href="/kapcsolat">Kapcsolat</a>
+                    </li>
+                    <li>
+                        <a href="/munkavedelem">Munkavédelem</a>
+                    </li>
+                    <li>
+                        <a href="/tuzvedelem">Tűzvédelem</a>
+                    </li>
+                    <li>
+                        <a href="/kornyezetvedelem">Környezetvédelem</a>
+                    </li>
+                    <li>
+                        <a href="/referenciak">Referenciák</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
