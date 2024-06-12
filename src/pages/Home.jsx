d88a85d509dc3c678a551b7900f30d084c56f0cd
Papp Dániel <pappdani2@gmail.com>
2024-06-12 15:57:25 +0200

    new components added to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Carousel from "../components/Carousel";
+import CardContainer from "../components/CardContainer";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import References from "../components/References";
 
 const Home = () => {
 
     return (
         <>
-            <Carousel/>
-            <div>
-            </div>
+            <Carousel />
+            <CardContainer />
+            <Contact />
+            <About />
+            <References />
         </>
 
     )
