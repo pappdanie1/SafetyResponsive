8fc433cd762b8f86db2c70fa43d9de918a02dd27
Papp Dániel <pappdani2@gmail.com>
2024-06-14 15:37:40 +0200

    References page implemented

---
+++ src/pages/ReferencesPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import References from "../components/References";
+
+const ReferencesPage = () => {
+
+    return (
+        <div className="ref-div" >
+            <References />
+        </div>
+    )
+}
+
+export default ReferencesPage
