16594c0aa1c973be20c5051b5facc92aed1b6290
Daniel Papp <pappdani2@gmail.com>
2026-02-24 16:21:05 +0100

    Optimize image load

--- src/components/SafetyCard.jsx
+++ src/components/SafetyCard.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import "../css/SafetyCard.css"
 
 const SafetyCard = (props) => {
 
     return (
         <div className="safety-card">
-            <img className="card-img" src={props.img} alt="" />
+            <LazyLoadImage
+                className="card-img"
+                src={props.img}
+                alt={props.head || ""}
+                width="100%"
+                height="100%"
+                effect="blur"
+                wrapperClassName="card-img-wrapper"
+            />
             <div>
                 <h2 className="safety-h2" >
                     {props.head}
